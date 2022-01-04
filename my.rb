59ba63c12c33c613939ae76215844c34bf3b21eb
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-04 19:25:05 +0530

    Exercise 5-study drill1
    Change all the variables so there is no my_ in front of each one in study drill 1

---
+++ my.rb
@@ -0,0 +1,14 @@
+name='ABC'
+age=30
+height=60
+weight=100
+eyes='Brown'
+teeth='White'
+hair='Brown'
+puts "Let's talk about #{name}."
+puts "He's #{height}inches tall."
+puts "He's #{weight}pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes}eyes and #{hair}hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+puts "If i add #{age},#{height}, and #{weight} I get #{age +height +weight}"
