d1d7c20f13a8c1c51eaf61b757b8e2323e857b5b
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-04 13:41:38 +0530

    Exercise 3-study drill 1
    comments for exercise 3-study drill 1

---
+++ hen.rb
@@ -0,0 +1,14 @@
+puts "I will count my chickens:"  # print statement
+puts "Hens #{25 + 30 / 6}"         #count the number of Hens using  arthimetic operations inside the curly braces
+puts "Roosters #{100 - 25 * 3 %4}"#count the number of Roosters using arthimetic operations inside the curly braces
+puts "Now i will count the eggs:"#print statement
+puts  3 + 2+ 1 - 5 + 4 % 2 - 1 / 4 +6 #perform the arthimetic operations
+puts "Is it true that 3+2<5-7?"  #print statement
+puts  3 + 2 < 5 - 7    #compare whether 3+2 is less than 5-7 and print the result
+puts "what is 3 + 2? #{3 + 2}"# print a statement with string interpolation
+puts "what is 5-7? #{5 - 7}"# print a statement with string interpolation
+puts "Oh,that's why it's false." # print statement
+puts "How about some more." # print statement
+puts "Is it greater? #{5 > -2}" # print a string statement and compare whether 5 is greater than -2 and print the result
+puts "Is it greater or equal? #{5 >= -2}"# print a statement and compare whether 5 is greater than or equal to -2 and print the result
+puts "Is it less or equal? #{5 <= -2}"# print a statement and compare whether 5 is less than or equal to -2 and print result
