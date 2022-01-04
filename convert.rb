6b77fc3a0fc522623d7caab282bab04eb4621ae7
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-04 19:33:37 +0530

    Exercise 5-study drill 2
    convert the inches and pounds to kilograms

---
+++ convert.rb
@@ -0,0 +1,18 @@
+name='ABC'
+age=28
+height=70
+height_in_centimeters="#{height*2.54}"
+weight=180
+weight_in_kilograms="#{weight/2.205}"
+eyes='Blue'
+teeth='White'
+hair='Brown'
+puts "Let's talk about #{name}."
+puts "He's #{height}inches tall."
+puts "#{height_in_centimeters}centimeter tall."
+puts "He's #{weight}pounds heavy."
+puts "#{weight_in_kilograms}kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes}eyes and #{hair}hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+puts "If i add #{age},#{height}, and #{weight} I get #{age +height +weight}"
