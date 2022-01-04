d68eb668a56fe00a98ad6ce17652fe3dd0f44ce9
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-04 11:39:59 +0530

    Exercise 2
    Exercise 2  code

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+# A comment,this is so you can read your program later.
+#Anything after the # is ignored by ruby
+puts " hello ruby" # The comment after is ignored
+# puts"ABC"
+puts"Welcome to ruby"
