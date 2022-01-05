2cf016e915893f333a0c78a38204f9e12e63e681
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-05 15:53:17 +0530

    Exercise 3-study drill 4

---
+++ floatinghen.rb
@@ -0,0 +1,13 @@
+puts "I will count my chickens:"
+puts "Hens #{25.0 + 30 / 6}"
+puts "Roosters #{100 - 25.0 *3 %4}"
+puts "Now i will count the eggs:"
+puts  3.0 + 2+ 1- 5 + 4 % 2 - 1 / 4 + 6
+puts  3 + 2.0 < 5- 7.0
+puts "what is 3 + 2? #{3 + 2.0}"
+puts "what is 5-7? #{5.0 - 7}"
+puts "Oh,that's why it's false."
+puts "How about some more."
+puts "Is it greater? #{5.0> -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5.0 <= -2}"
