3db8737642cf16716039bf8ce342bf8322ef0fad
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-05 16:24:47 +0530

    Exercise 6-study drill 1
    Comments for  exercise 6-study drill 1

---
+++ people.rb
@@ -0,0 +1,15 @@
+types_of_people =10 #this variable denotes types of people
+x = "There are #{types_of_people}types of people."#string interpolation
+binary = "binary" # assigns binary variable is equal to string with double quotes
+do_not ="don't"#assigns don't variable is equal to string with double quotes
+y = "Those who know #{binary} and those who #{do_not}"#printing the result
+puts x #puts string
+puts y #puts string
+puts "I said: #{x}."#printing the result
+puts "I also said:'#{y}'."#printing the result
+hilarious = false #assigns hilarious equals to false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+puts joke_evaluation #print statement
+w = "This is the left side of..."# the value of w
+e = "A string with a right side."#the value of e
+puts w + e #concatenate w and e
