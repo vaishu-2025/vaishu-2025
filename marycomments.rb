d3dea72ba1abd03c112261fc364bcaf6eff688aa
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-06 11:02:16 +0530

    Exercise7-study drill 1
    Comments for exercise 7-study drill

---
+++ marycomments.rb
@@ -0,0 +1,19 @@
+puts "Marry had a little lamb." # put string
+puts "Its fleece was white as #{'snow'}."# put string
+puts "And everywhere that Mary went."#print statement
+puts "." * 10  #print "." 10 times
+end1 = "c" #this variable assignment operator denotes end1 = c
+end2 = "h" #this variable assignment operator denotes end2 = h
+end3 = "e" #this variable assignment operator denotes end3 = e
+end4 = "e" #this variable assignment operator denotes end4 = e
+end5 = "s" #this variable assignment operator denotes end5 = s
+end6 = "e" #this variable assignment operator denotes end6 = e
+end7 = "b" #this variable assignment operator denotes end7 = b
+end8 = "u" #this variable assignment operator denotes end8 = u
+end9 = "r" #this variable assignment operator denotes end9 = r
+end10 = "g"#this variable assignment operator denotes end10 = g
+end11 = "e"#this variable assignment operator denotes end11 = e
+end12 = "r"#this variable assignment operator denotes end12 = r
+print end1 + end2 + end3 + end4 + end5 + end6 # concatenates the variables end1....end6
+puts end8 + end9 + end10 + end11 + end12 #concatenates the variable end7...end12
+#concatenates these variable on the same line
