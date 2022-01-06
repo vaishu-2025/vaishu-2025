5ce3b85983cc581a4fa41de2f74658b8ed434451
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-06 18:59:01 +0530

    Exercise9-study drill1
    Code for exercise9-study dril1

---
+++ exe9.rb
@@ -0,0 +1,10 @@
+days = "Mon Tue Wed Thu Fri Sat Sun" #It display week of a days in a same line
+months = "jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #\n denotes a new line
+puts "Here are the days: #{days}"#print statement
+puts " Here are the months: #{months}"# print statement
+puts %q{ #%q behaves like a single quotes string
+There is something going on here.
+With the three double-quotes.
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
