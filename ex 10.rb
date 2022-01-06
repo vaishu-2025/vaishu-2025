a8bdad30581b17f02c244992e16419763caef0d9
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-06 19:16:06 +0530

    Exercise 10
    Code for exercise 10

---
+++ ex 10.rb
@@ -0,0 +1,13 @@
+tabby_cat= "\t I'm tabbed in."
+Persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* cat food
+\t* Fishies
+\t* catnip\n\t* Grass
+"""
+puts tabby_cat
+puts Persian_cat
+puts backslash_cat
+puts fat_cat
