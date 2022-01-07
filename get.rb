0511fdf5377b3ecac9c5e5f02d660b0f7d14fca0
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-07 13:55:31 +0530

    Exercise 12-study drill 1
    The to_f function in Ruby converts the value of the number as a float.

---
+++ get.rb
@@ -0,0 +1,6 @@
+puts "Enter first value: "
+num1 = gets.chomp.to_f
+puts "Enter second value: "
+num2 = gets.chomp.to_f
+sum = num1 + num2 # finding sum
+puts "The sum is #{sum}"# printing the result
