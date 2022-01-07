9ccf8c7daab88a598f442a9493ad6f963d889b54
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-07 12:58:09 +0530

    Exercise 11- study drill 3

---
+++ form.rb
@@ -0,0 +1,11 @@
+puts " Enter employee name:"
+name = gets.chomp
+puts "Employee gender"
+gender = gets.chomp
+puts "employee joining date:"
+date = gets.chomp
+puts "Employee age:"
+age = gets.chomp
+puts "Employee salary package:"
+salary package = gets.chomp
+puts " you are #{name},and you are #{gender} your joining date is #{date} and your age is now #{age} and your salary is #{salary package}."
