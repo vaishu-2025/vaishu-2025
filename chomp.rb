f390f094d264b6e067d56ca776ed6466d1c65537
vaishu-2025 <96916250+vaishu-2025@users.noreply.github.com>
2022-01-07 12:04:47 +0530

    Exercise 11-study drill 1
    Example code for study drill 1

---
+++ chomp.rb
@@ -0,0 +1,8 @@
+#without using chomp method
+puts "Enter your name"
+str = gets
+puts "Hi "+str+"Welcome to Chennai."
+#using chomp method
+puts "Enter your name"
+str=gets.chomp
+puts "Hi "+str+" Welcome to Chennai."
